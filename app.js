e993a3f0c539b2d0ae521e418703aa7e2e703c84
natalia <nataliamodiano@gmail.com>
2015-12-01 19:24:00 -0800

    Made navbar and hero responsive

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 /*hide and show login prompt*/
-function showClick() {
-  var showEl = document.getElementById('hidden');
-  div.setAttribute('hidden', 'show')
+
+function clicked(button, toggle) {
+ if (toggle === 'collapse') {
+    addEventListener('clicked',function() {
+      class.setAttribute('collapse', 'in');
+    }, false)
+ }
+ else if (toggle === 'in') {
+    addEventListener('clicked',function() {
+      class.setAttribute('in', 'collapse');
+    }, false)
+ }
+ else {
+   console.log('broken');
+ }
 }
 
-var navLogin = document.getElementById('log-in');
-var btn = document.getElementsByTagName('btn')
-navLogin.addEventListener('click', showClick, false);
+
+var loginButton = getElementById('login');
+var login = getElementById('log-in');
+clicked(loginButton, login[1]);
+
